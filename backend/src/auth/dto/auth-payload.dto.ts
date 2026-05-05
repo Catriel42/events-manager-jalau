0be22eb5522f28d2e78ce93591194199a1266968
Catriel Pereira <catopereira72@gmail.com>
2026-05-04 22:17:02 -0400

    feat(auth): define auth payload and strictly typed request interfaces

---
+++ backend/src/auth/dto/auth-payload.dto.ts
@@ -0,0 +1,14 @@
+import { Request } from 'express';
+import { UserEntity } from '@users/dto/user.interface';
+
+export interface AuthPayload {
+  sub: string;
+  email: string;
+}
+
+export interface RequestWithUser extends Request {
+  user: {
+    user: UserEntity;
+    accessToken: string;
+  };
+}
