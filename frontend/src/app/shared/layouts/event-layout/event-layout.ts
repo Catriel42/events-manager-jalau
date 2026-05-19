b55975989955650f1f9a09eb0371b070f42dbf75
Catriel Pereira <catopereira72@gmail.com>
2026-05-18 21:18:53 -0400

    feat: adding event footer and changing logos

--- frontend/src/app/shared/layouts/event-layout/event-layout.ts
+++ frontend/src/app/shared/layouts/event-layout/event-layout.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-event-layout',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, RouterLink, Navbar],
   templateUrl: './event-layout.html',
 })
 export class EventLayout {}
